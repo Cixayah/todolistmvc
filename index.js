7bdfc1548b7571671dbf7fec9a4376cc39fdcda3
Cix <gabrielviniciodacosta@gmail.com>
2024-01-26 09:59:32 -0300

    #

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const port = 3000;
-app.set('view engine','ejs')
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
 });
