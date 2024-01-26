205da60cefa6b689899b395c469f2ef5a084bfb7
Cix <gabrielviniciodacosta@gmail.com>
2024-01-26 09:35:57 -0300

    #

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 const server = app.listen(3000, () => {
-    console.log('Server ativo na porta 3000')
-})
-
+  console.log("Server ativo na porta 3000");
+});
