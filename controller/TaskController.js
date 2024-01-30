bb72787476835f0e87ebd1005d043a074441b141
Cix <gabrielviniciodacosta@gmail.com>
2024-01-30 15:02:21 -0300

    #

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -1,25 +1,40 @@
 const Task = require("../models/Task");
+let message = "";
+let type = "";
 
 const getAllTasks = async (req, res) => {
   try {
+    setTimeout(() => {
+      message = "";
+    }, 1000);
     const tasksList = await Task.find();
-    return res.render("index", { tasksList, task: null, taskDelete: null });
+    return res.render("index", {
+      tasksList,
+      task: null,
+      taskDelete: null,
+      message,
+      type,
+    });
   } catch (err) {
-    res.status(500).send({ error: err.massage });
+    res.status(500).send({ error: err.message });
   }
 };
 
 const createTask = async (req, res) => {
   const task = req.body;
   if (!task.task) {
+    message = "Insira um texto antes de adicionar a terefa!";
+    type = "danger";
     return res.redirect("/");
   }
 
   try {
     await Task.create(task);
+    message = "Tarefa criada com sucesso!";
+    type = "success";
     return res.redirect("/");
   } catch (err) {
-    res.status(500).send({ error: err.massage });
+    res.status(500).send({ error: err.message });
   }
 };
 
@@ -28,13 +43,29 @@ const getById = async (req, res) => {
     const tasksList = await Task.find();
     if (req.params.method == "update") {
       const task = await Task.findOne({ _id: req.params.id });
-      res.render("index", { task, taskDelete: null, tasksList });
+      res.render("index", {
+        task,
+        taskDelete: null,
+        tasksList,
+        message,
+        type,
+      });
     } else {
-      const taskDelete = await Task.findOne({ _id: req.params.id });
-      res.render("index", { task: null, taskDelete, tasksList });
+      const taskDelete = await Task.findOne({
+        _id: req.params.id,
+      });
+      res.render("index", {
+        task: null,
+        taskDelete,
+        tasksList,
+        message,
+        type,
+      });
     }
   } catch (err) {
-    res.status(500).send({ error: err.massage });
+    res.status(500).send({
+      error: err.message,
+    });
   }
 };
 
@@ -42,18 +73,40 @@ const updateOneTask = async (req, res) => {
   try {
     const task = req.body;
     await Task.updateOne({ _id: req.params.id }, task);
+    message = "Tarefa atualizada com sucesso!";
+    type = "success";
     res.redirect("/");
   } catch (err) {
-    res.status(500).send({ error: err.massage });
+    res.status(500).send({ error: err.message });
   }
 };
 
 const deleteOneTask = async (req, res) => {
   try {
     await Task.deleteOne({ _id: req.params.id });
-    res.redirect("/")
+    message = "Tarefa apagada com sucesso!";
+    type = "success";
+    res.redirect("/");
   } catch (err) {
-    res.status(500).send({ error: err.massage });
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const taskCheck = async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+    //Condicional ternária
+    task.check ? (task.check = false) : (task.check = true);
+    // if (task.check) {
+    //   /*.check já é um boolean*/
+    //   task.check = false;
+    // } else {
+    //   task.check = true;
+    // }
+    await Task.updateOne({ _id: req.params.id }, task);
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
   }
 };
 
@@ -63,4 +116,5 @@ module.exports = {
   getById,
   updateOneTask,
   deleteOneTask,
+  taskCheck,
 };
